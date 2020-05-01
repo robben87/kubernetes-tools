202c41c95595e1308803f9662637d38bb8915adb
root <root@dev-ldocflykubem01.esec.local>
2020-05-01 16:49:46 +0200

    decode-secrets

--- kubectl-tool.py
+++ kubectl-tool.py
@@ -81,17 +81,11 @@ def scaledeploy(namespace,actual_replicas,replicas):
 
     Template = os.path.join(WorkDir,"template.yaml")
     List = os.path.join(WorkDir,"lista.deploy.txt")
-   #DEBUG  print (Template)
-   #DEBUG  print (List)
     try:
-        #Create WorkDir
         if os.path.exists(WorkDir) :
-            # DEBUG Abilitare il debug, inutile printare sempre questo output <----
-            #print("--WorkDir %s exists, continue ..." %(WorkDir))
             print("")
         else:
             os.makedirs(WorkDir)
-        #Clean Old files
         if os.path.exists(Template):
             os.remove(Template)
         if os.path.exists(List):
@@ -101,8 +95,6 @@ def scaledeploy(namespace,actual_replicas,replicas):
 
     cmd=('kubectl get deployments -n %s  | grep "%s/%s" | awk \'{print $1}\' > %s' % (args.namespace,args.actualreplicas,args.actualreplicas,List))
     os.system(cmd)
-    #print ("-----------LEGGO FILE-------------")
-
     #\\ Template File creation \\#
     file=open(Template,"w")
     file.write("apiVersion: extensions/v1beta1\n")
@@ -112,7 +104,6 @@ def scaledeploy(namespace,actual_replicas,replicas):
     file.write("spec:\n")
     file.write("  replicas: {{ replicas }}\n")
     file.close()
-
     with open(List) as name:
         for line in name:
             deploy_name=line.replace('\n','')
@@ -129,11 +120,9 @@ def scaledeploy(namespace,actual_replicas,replicas):
             j2_env = Environment(loader=FileSystemLoader(THIS_DIR),trim_blocks=True)
             output=j2_env.get_template('template.yaml').render(name=deploy_name,replicas=args.replicas)
             file=open(patch_file,"w")
-            #print(file)
             file.write(str(output))
             file.close()
             cmd=('kubectl patch deployments/%s --patch "$(cat %s)" -n %s'%(deploy_name,patch_file,args.namespace))
-            #print("----Scaling deployment----")
             os.system(cmd)
 
     if os.path.exists(WorkDir) and os.path.isdir(WorkDir):
@@ -144,17 +133,12 @@ def RollingDeploy():
 
    Template = os.path.join(WorkDir,"template.yaml")
    List = os.path.join(WorkDir,"lista.deploy.txt")
-   #DEBUG  print (Template)
-   #DEBUG  print (List)
    try:
        #Create WorkDir
        if os.path.exists(WorkDir) :
-           # DEBUG Abilitare il debug, inutile printare sempre questo output <----
-           #print("--WorkDir %s exists, continue ..." %(WorkDir))
            print("")
        else:
            os.makedirs(WorkDir)
-       #Clean Old files
        if os.path.exists(Template):
            os.remove(Template)
        if os.path.exists(List):
@@ -203,17 +187,24 @@ def RollingDeploy():
    if os.path.exists(WorkDir) and os.path.isdir(WorkDir):
        shutil.rmtree(WorkDir)
 
-def DecodeSecrets():
+def decodesecrets():
     v1 = client.CoreV1Api()
 
     if all([ args.namespace ,args.secret ]):
         secrets = v1.read_namespaced_secret(name=args.secret,namespace=args.namespace,pretty='True')
         print ("\n------- Decoding Secret: "+secrets.metadata.name.upper()+" -------\n")
         secrets_data = (secrets.data)
+        data = pd.DataFrame(columns=['KEY','VALUE'])
         for key,value in secrets_data.items():
             value_decoded = base64.b64decode(value)
-            keys = str(key+":")
-            print("%-15s\t%-15s" % (keys,value_decoded.decode('utf-8',errors='ignore')))
+            value_decoded = value_decoded.decode('utf-8',errors='ignore')
+            #keys = str(key+":")
+            data = data.append({'KEY': key,'VALUE': value_decoded}, ignore_index=True)
+            
+            #output = output.str.wrap(50)
+            #print("%-15s\t%-15s" % (keys,value_decoded.decode('utf-8',errors='ignore')))
+        output = data.to_string(justify='left',index=False)
+        print(output)
         sys.exit()
 
     if  args.namespace:
@@ -234,7 +225,6 @@ if __name__=='__main__':
 
     #Call Functions:
     EnvDefinition()
-
     #Argparse e help menu
     #createthetop-levelparser
     parser=argparse.ArgumentParser()
@@ -248,12 +238,18 @@ if __name__=='__main__':
     parser_view.add_argument("-sc","--scale", help="Scaling down, if defined then will scale to specified replicas arg --replicas",action="store",required=False,nargs='?', const="Y", type=str)
     parser_view.set_defaults(func=viewdeploy)
 
-   # #createtheparserforthe"scale"command
-   # parser_scale=subparsers.add_parser("scale",help="Scale deployments at desired replicas")
-   # parser_scale.add_argument("-n","--namespace",help="Specify namespace",default="default")
-   # parser_scale.add_argument("-r","--replicas", help="Specify for which replicas you want to scale",required=False)
-   # parser_scale.add_argument("-ar","--actualreplicas", help="Search for deployments that has same int value of replica",action="store",required=False)
-   # parser_scale.set_defaults(func=scaledeploy)
+    #createtheparserforthe"scale"command
+    parser_scale=subparsers.add_parser("scale",help="Scale deployments at desired replicas")
+    parser_scale.add_argument("-n","--namespace",help="Specify namespace",default="default")
+    parser_scale.add_argument("-r","--replicas", help="Specify for which replicas you want to scale",required=False)
+    parser_scale.add_argument("-ar","--actualreplicas", help="Search for deployments that has same int value of replica",action="store",required=False)
+    parser_scale.set_defaults(func=scaledeploy)
+
+#    #createtheparserforthe"events"command
+#    parser_events=subparsers.add_parser("events",help="View events ordered by time")
+#    parser_events.add_argument("-w","--watch",help="tail events",default=None,required=False,action='store_true')
+#    parser_events.add_argument("-n","--namespace",help="specify namespace",default=None,required=False)
+#    parser_events.set_defaults(func=GetEvents)
 
     #createtheparserforthe"RollingUpdate"command
     parser_rolling=subparsers.add_parser("rolling-update",help="Execute rolling Update for deployments")
@@ -265,7 +261,7 @@ if __name__=='__main__':
     parser_decode=subparsers.add_parser("decode-secrets",help="Decode secrets contents")
     parser_decode.add_argument("-s","--secret",help="Secret name to be decoded",action="store",required=False)
     parser_decode.add_argument("-n","--namespace",help="Specify namespace. Without secret specified it will print all secrets decoded for the namespace",default="default")
-    parser_decode.set_defaults(func=DecodeSecrets)
+    parser_decode.set_defaults(func=decodesecrets)
 
     if len(sys.argv[1:])==0:
         parser.print_help()
@@ -274,11 +270,15 @@ if __name__=='__main__':
 
     if "view" in sys.argv:
         viewdeploy()
-    #elif "scale" in sys.argv:
-    #    scaledeploy()
+    elif "scale" in sys.argv:
+        scaledeploy()
+    # TODO
+    # elif "events" in sys.argv:
+        # GetEvents()
     elif "rolling-update" in sys.argv:
         RollingDeploy()
     elif "decode-secrets" in sys.argv:
-        DecodeSecrets()
+        decodesecrets()
     else:
         print("nulla")
+
